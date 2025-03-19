c764201bd34f2340934276ff099b3b6263edc696
eswaraprasad <esweraprasad@gmail.com>
2025-03-19 11:35:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route,} from 'react-router-dom'
+const App = () => {
+
+  return (
+    <>  
+    <Routes>
+      <Route path='/home' />
+    </Routes>
+    </>
+  )
+}
+
+export default App
